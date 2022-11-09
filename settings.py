af62a15492aa09a31af5d9f2243fdb2bac7963ed
antoinettehemlall <hemlall3000@gmail.com>
2022-11-09 12:49:04 -0500

    Added functionality of bullets.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (0, 0, 0)
 
         #Ship settings (1.5 pixels)
         self.ship_speed = 1.5
