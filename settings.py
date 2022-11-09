c3df1e851f6321576126ba0f27e3901d4fc65eaa
antoinettehemlall <hemlall3000@gmail.com>
2022-11-09 13:11:34 -0500

    Refactored code for bullet position.

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0, 0, 0)
+        self.bullets_allowed = 3
 
         #Ship settings (1.5 pixels)
         self.ship_speed = 1.5
